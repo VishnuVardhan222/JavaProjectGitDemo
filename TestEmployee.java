c038c5fe5aade439ecc578fe236fb19fe9ac4495
vishnu <vvvarma2001@gmail.com>
2022-04-11 12:08:28 +0530

    firstCommit

---
+++ TestEmployee.java
@@ -0,0 +1,10 @@
+public class TestEmployee{
+	public static void main(String[] args)
+	{
+		Employee e = new Employee(1244,"vishnu","developer",40000);
+		System.out.println(e.getName());
+		
+	}
+
+
+}
